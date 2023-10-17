0b988f86bedc43d67b0ab9038b78277f95842a67
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-10-17 13:19:39 -0400

    Add math_question class

---
+++ math_question.rb
@@ -0,0 +1,17 @@
+class Math_question
+  attr_reader
+
+  def initialize
+    generate_question
+  end
+
+  def generate_question
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+  end
+
+  def check_answer(answer)
+    answer.to_i  == @num1 + @num2
+  end
+  
+end
