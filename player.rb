aa391c4d60b7d85c0d87e285ff1d82ec495f6611
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-10-17 13:19:25 -0400

    Add player class

---
+++ player.rb
@@ -0,0 +1,13 @@
+class Player
+  attr_accessor :name, :number_of_lives
+
+  def initialize(name)
+    @name = name
+    @number_of_lives = 3
+  end
+
+  def decrease_life
+    @number_of_lives -= 1
+  end
+
+end
